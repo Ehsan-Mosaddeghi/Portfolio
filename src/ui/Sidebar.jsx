b174770dbb2d400dc07a13bcf3b38c2bcf77787e
Ehsan <xdehsanxd@gmail.com>
2024-09-10 22:15:30 +0330

    Update Sidebar.jsx

--- src/ui/Sidebar.jsx
+++ src/ui/Sidebar.jsx
@@ -3,7 +3,7 @@ import NavbarItem from "./NavbarItem";
 
 function Sidebar({ items }) {
   return (
-    <div className="absolute top-0 z-10 w-full animate-bounce space-y-4 bg-stone-800 px-20 pb-4 pt-[4.5rem] text-primary sm:hidden">
+    <div className="fixed top-0 z-10 w-full animate-bounce space-y-4 bg-stone-800 px-20 pb-4 pt-[4.5rem] text-primary sm:hidden">
       {items.map((item) => (
         <NavbarItem item={item} key={item.name} />
       ))}
