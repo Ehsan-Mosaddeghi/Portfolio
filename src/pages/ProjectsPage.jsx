e88cfa28efce93ccd88c97ae3b18430f2db3f5bc
Ehsan <xdehsanxd@gmail.com>
2024-09-20 21:01:22 +0330

    add Project

--- src/pages/ProjectsPage.jsx
+++ src/pages/ProjectsPage.jsx
@@ -10,7 +10,19 @@ function ProjectsPage() {
       <h1 className="mb-8 text-3xl">{t("Projects")}</h1>
 
       <ul className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-        <ProjectItem />
+        <ProjectItem
+          image="/images/projects_todo.png"
+          title="To Do list website"
+          description="Created with pure HTML & CSS and JavaScript"
+          href="https://ehsan-mosaddeghi.github.io/Todo-list-app/"
+        />
+
+        <ProjectItem
+          image="/images/projects_worldwise_user.png"
+          title="The World Wise"
+          description="Created with NextJS, TailwindCSS and Supabase."
+          href="https://the-wild-oasis-user-ten.vercel.app/"
+        />
       </ul>
     </AnimatePage>
   );
