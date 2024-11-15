840821fee12b1d815831584080b536073ce284d4
Ehsan <xdehsanxd@gmail.com>
2024-11-15 13:15:37 +0330

    Edit footer background color

--- src/ui/Footer.jsx
+++ src/ui/Footer.jsx
@@ -6,23 +6,28 @@ function Footer() {
   const { t } = useTranslation();
 
   return (
-    <footer className="flex border-b-2 border-blue-500 bg-[#00000050]">
-      <div className="container flex w-full items-center justify-between">
-        <div className="max-w-lg space-y-3">
-          <h2>{t("Frontend developer")}</h2>
-          <p>
-            {t("© 2024 Ehsan Mosaddeghi, Tabriz, Iran. All rights reserved.")}
-          </p>
+    <>
+      <footer className="flex bg-[#00000050]">
+        <div className="container flex w-full items-center justify-between">
+          <div className="max-w-lg space-y-3">
+            <h2>{t("Frontend developer")}</h2>
+            <p>
+              {t(
+                "© 2024 Ehsan Mosaddeghi, Tabriz, Iran. All rights reserved.",
+              )}
+            </p>
+          </div>
+          <div className="flex flex-col justify-center gap-[0.6rem] text-center text-xl">
+            <h4>{t("Socials")}</h4>
+            <Links />
+            <ResumeButton href="/Ehsan-Mosaddeghi-Resume.pdf">
+              {t("My Resume")}
+            </ResumeButton>
+          </div>
         </div>
-        <div className="flex flex-col justify-center gap-[0.6rem] text-center text-xl">
-          <h4>{t("Socials")}</h4>
-          <Links />
-          <ResumeButton href="/Ehsan-Mosaddeghi-Resume.pdf">
-            {t("My Resume")}
-          </ResumeButton>
-        </div>
-      </div>
-    </footer>
+      </footer>
+      <div className="block h-1 w-full bg-[linear-gradient(90deg,#24255537,#585ac7,#24255542)]"></div>
+    </>
   );
 }
 
