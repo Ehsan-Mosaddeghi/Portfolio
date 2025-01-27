b3967e7680941308c01b498f5e944b49dc60bac8
Ehsan <xdehsanxd@gmail.com>
2025-01-27 14:44:44 +0330

    Refactor AppLayout component to remove dark mode background logic and apply gradient background

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -4,15 +4,13 @@ import Navbar from "./Navbar";
 import Footer from "./Footer";
 
 import { useTranslation } from "react-i18next";
-import { useDarkMode } from "../context/DarkModeContext";
 
 function AppLayout() {
   const { i18n } = useTranslation();
-  const { isDarkMode } = useDarkMode();
 
   return (
     <div
-      className={`flex min-h-svh flex-col from-from to-to bg-cover bg-center bg-no-repeat text-primary transition-all duration-[400ms] ${i18n.language === "fa" ? "text-right font-Rubik" : "font-sans"} ${isDarkMode ? "bg-[url(/wave-haikei.svg)]" : "bg-[url(/wave-haikei-light.svg)]"}`}
+      className={`flex min-h-svh flex-col bg-gradient-to-b from-from to-to bg-cover bg-center bg-no-repeat text-primary transition-all duration-[400ms] ${i18n.language === "fa" ? "text-right font-Rubik" : "font-sans"}`}
     >
       <Navbar />
       <section className="container m-auto mt-12 flex-auto sm:mt-[4.5rem]">
